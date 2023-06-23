1506c043690bd2cc651d993e72420b3510bbcba8
Segfault1602 <stonge.alex@gmail.com>
2023-06-23 11:10:16 -0700

    Update build to run on mac + ubuntu

---
+++ tools/player/gamepad_dummy.cpp
@@ -0,0 +1,25 @@
+#include "gamepad.h"
+
+class Gamepad::Impl
+{
+  public:
+    Impl(){}
+
+    ~Impl(){}
+
+    bool Poll(GamepadState& state)
+    {
+        return false;
+    }
+};
+
+Gamepad::Gamepad() : impl_(std::make_unique<Impl>())
+{
+}
+
+Gamepad::~Gamepad() = default;
+
+bool Gamepad::Poll(GamepadState& state)
+{
+    return impl_->Poll(state);
+}
